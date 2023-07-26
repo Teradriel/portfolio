cc4209c8ef333f89769b861cff0780c1345d68af
Teradriel <luca.terzariol@outlook.com>
2023-07-26 22:59:57 +0200

    added CV and downloads - updated translation

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  lang: string =this._translocoService.getActiveLang();
+  url =""
+  constructor(private _translocoService: TranslocoService) { }
 
   ngOnInit(): void {
+    this._translocoService.langChanges$.subscribe((response) => {
+      this.lang = response
+    })}
+
+  public key(){
+    if (this.lang === "en") {
+      let link = document.createElement('a')
+      link.href = 'https://en.wikipedia.org/wiki/Keystone_(architecture)'
+      link.target ='_blank'
+      link.click()
+    }
+    if(this.lang === "it"){
+      let link = document.createElement('a')
+      link.href = 'https://it.wikipedia.org/wiki/Chiave_di_volta'
+      link.target ='_blank'
+      link.click()
+    }
+    if(this.lang === "es"){
+      let link = document.createElement('a')
+      link.href = 'https://es.wikipedia.org/wiki/Clave_(arquitectura)'
+      link.target ='_blank'
+      link.click()
+    }
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
+import { NgbPopoverConfig, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbPopoverConfig],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  lang: string =this._translocoService.getActiveLang();
+
+  constructor(private _translocoService: TranslocoService, config: NgbPopoverConfig) {
+    config.placement = 'bottom';
+		config.triggers = 'hover';
+   }
 
   ngOnInit(): void {
+    this._translocoService.langChanges$.subscribe((response) => {
+      this.lang = response
+    })
+
+    }
+
+  public cvIng(){
+    if (this.lang === "en") {
+      let link = document.createElement('a')
+      link.href = '../../../assets/CV/[EN] CV TERZARIOL LUCA.pdf'
+      link.click()
+    }
+    if(this.lang === "it"){
+      let link = document.createElement('a')
+      link.href = '../../../assets/CV/[IT] CV TERZARIOL LUCA.pdf'
+      link.click()
+    }
+    if(this.lang === "es"){
+      let link = document.createElement('a')
+      link.href = '../../../assets/CV/[ES] CV TERZARIOL LUCA.pdf'
+      link.click()
+    }  
+  }
+
+  public cvDev(){
+    if (this.lang === "en") {
+      let link = document.createElement('a')
+      link.href = '../../../assets/CV/[EN] CV TERZARIOL LUCA (DEV).pdf'
+      link.click()
+    }
+    if(this.lang === "it"){
+      let link = document.createElement('a')
+      link.href = '../../../assets/CV/[IT] CV TERZARIOL LUCA (DEV).pdf'
+      link.click()
+    }
+    if (this.lang === "es"){
+      let link = document.createElement('a')
+      link.href = '../../../assets/CV/[ES] CV TERZARIOL LUCA (DEV).pdf'
+      link.click()
+    }
   }
 
 }
